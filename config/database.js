4772f2067e8b18a3a6c56743b215f644e2b370a1
Nam Ninja <namninja@gmail.com>
2019-03-09 12:52:35 -0800

    integrating with Travis CI and deploying to Heroku

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 "use strict";
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://zero-user:zero-user@cluster0-pfvwv.mongodb.net/bee-well-journal?retryWrites=true';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb+srv://zero-user:zero-user@cluster0-pfvwv.mongodb.net/test-bee-well-journal?retryWrites=true';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://zero-admin:zero-admin@cluster0-pfvwv.mongodb.net/bee-well-journal?retryWrites=true';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb+srv://zero-admin:zero-admin@cluster0-pfvwv.mongodb.net/test-bee-well-journal?retryWrites=true';
 exports.PORT = process.env.PORT || 8080;
