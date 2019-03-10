13b1fd7e3527ffe55680000ad9d6bd48786424f0
Nam Ninja <namninja@gmail.com>
2019-03-10 15:33:15 -0700

    deploy to heroku

--- app/journal-router.js
+++ app/journal-router.js
@@ -56,7 +56,7 @@ router.get('/create-journal', isLoggedIn, function (req, res) {
 
 
 
-router.post('/create-journal',  jsonParser, function(req, res) {
+router.post('/create-journal',  isLoggedIn, jsonParser, function(req, res) {
     // const newEntry = new Journal(req.body)
     
     User.findById(req.user._id)
